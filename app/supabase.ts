e6562b683318db569b199211a9141ba51335fa83
Yasir Hareeri <Yasir.Hareeri@gmail.com>
2026-05-20 13:49:20 +0300

    fix supabase key

--- app/supabase.ts
+++ app/supabase.ts
@@ -4,7 +4,8 @@ const supabaseUrl =
   "https://sysdgjhtwuoatinjfieo.supabase.co";
 
 const supabaseKey =
-anon public key
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5c2Rnamh0d3VvYXRpbmpmaWVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4ODMyODMsImV4cCI6MjA5NDQ1OTI4M30.PQe702H7IcANch11OqqXHoFUb6gCO670HtSHZKbp53E";
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseKey
